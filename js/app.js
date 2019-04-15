07948d1daa3b47fef6c589bd20e7fe4206d479eb
pgiannac <pgiannac@hotmail.com>
2019-04-16 00:05:36 +0300

    updated with removeDups()

--- js/app.js
+++ js/app.js
@@ -2,37 +2,45 @@
 jQuery(init); // Same as: jQuery( document ).ready( init )
 
 // My app logic goes here:
-function init() {
+function init($) {
+
     let options = {
-        url: "data.json",
+        url: "../data.json",
         success: jsonHandler
     }
 
     function jsonHandler(data) {
-        // console.log(data);
+
         let entries = data[1].entries;
-        console.log(data); //Array [{hotel} x 4]
+        // console.log( entries ); 
+        let cities = entries.map(getCity); // Array[ { HOTEL } x 4 ]
 
-        let cities = entries.map( getCity );
-        console.log(cities);
-
-        function getCity( hotel ){
+        function getCity(hotel) { // 4 times: 1 { ... }, 2 { ... }, 3 { ... }, 4 { ... }
             return hotel.city;
         }
 
-        function removeDups(names){
-            let uniqueNames = {};
-            names.forEach(function(i))({
-                if(!uniqueNames[i]) { uniqueNames[i] = true; }
+        function removeDups(names) {
+            let unique = {
+                Paris: true,
+                Marseille: true,
+                Toulouse: true,
+            };
+            names.forEach(function (i) {
+                if (!unique[i]) { unique[i] = true; }
             });
-            return Object.keys(uniqueNames);
+            return Object.keys(unique);
         }
-        // document.createElement( TAG )
-        // ELEMENT.innerHTML
-        // ELEMENT.appendChild
-        // 'TEXT and dynamic ${values} from JS ${5+5}'
+
+        let uniqueCities = removeDups(cities);
+
+        console.log(uniqueCities);
+
     }
-    $.ajax(options);
+    // document.createElement( TAG )
+    // ELEMENT.innerHTML
+    // ELEMENT.appendChild
+    // 'TEXT and dynamic ${values} from JS ${5+5}'
+$.ajax(options);
 }
 
 // Alternative, no global vars at all:
