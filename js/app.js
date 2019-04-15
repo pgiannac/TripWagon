52fb5a99b70788dc9a7b015be2dc8382a9d8f1f4
pgiannac <pgiannac@hotmail.com>
2019-04-15 23:54:43 +0300

    updated with removeDups

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 
 jQuery(init); // Same as: jQuery( document ).ready( init )
 
-// Our app logic goes here:
-function init( $ ){
-    console.log("Ready");
+// My app logic goes here:
+function init() {
+    let options = {
+        url: "data.json",
+        success: jsonHandler
+    }
+
+    function jsonHandler(data) {
+        // console.log(data);
+        let entries = data[1].entries;
+        console.log(data); //Array [{hotel} x 4]
+
+        let cities = entries.map( getCity );
+        console.log(cities);
+
+        function getCity( hotel ){
+            return hotel.city;
+        }
+
+        function removeDups(names){
+            let uniqueNames = {};
+            names.forEach(function(i))({
+                if(!uniqueNames[i]) { uniqueNames[i] = true; }
+            });
+            return Object.keys(uniqueNames);
+        }
+        // document.createElement( TAG )
+        // ELEMENT.innerHTML
+        // ELEMENT.appendChild
+        // 'TEXT and dynamic ${values} from JS ${5+5}'
+    }
+    $.ajax(options);
 }
 
 // Alternative, no global vars at all:
