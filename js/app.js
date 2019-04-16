9c2743352fd8d1167a1afe336801414c197635cd
pgiannac <pgiannac@hotmail.com>
2019-04-16 15:56:37 +0300

    added function createOptions()

--- js/app.js
+++ js/app.js
@@ -2,37 +2,46 @@
 jQuery(init); // Same as: jQuery( document ).ready( init )
 
 // My app logic goes here:
-function init($) {
+function init( $ ){
 
     let options = {
         url: "../data.json",
         success: jsonHandler
     }
 
-    function jsonHandler(data) {
-        function getCity(hotel) { // 4 times: 1 { ... }, 2 { ... }, 3 { ... }, 4 { ... }
-            return hotel.city;
-        }
-        
-        function removeDups(names) {
-            let unique = {
-                Paris: true,
-                Marseille: true,
-                Toulouse: true,
-            };
-            names.forEach(function (i) {
-                if (!unique[i]) { unique[i] = true; }
-            });
-            return Object.keys(unique);
-        }
-        // console.log( entries ); 
-        let entries = data[1].entries;
-        let cities = entries.map(getCity); // Array[ { HOTEL } x 4 ]
-        let uniqueCities = removeDups(cities);
+    function removeDups(names) {
+        let unique = {};
+        names.forEach(function(i) {
+          if(!unique[i]) { unique[i] = true; }
+        });
+        return Object.keys(unique);
+    }
 
-        console.log(uniqueCities);
+    function createOptions( listOfCities ){
+
+        let datalist = document.querySelector( "#cities-list" );
+        listOfCities.map( addOption );
+
+        function addOption( city ){
+            datalist.innerHTML += `<option value="${ city }"></option>`;
+        }
 
     }
+
+    function jsonHandler( data ){
+
+        function getCity( hotel ){ 
+            return hotel.city;
+        }
+
+        let entries = data[1].entries;
+        let cities = entries.map( getCity ); 
+        let uniqueCities = removeDups( cities ); 
+        uniqueCities.sort();
+        createOptions( uniqueCities );
+
+    }
+
     // document.createElement( TAG )
     // ELEMENT.innerHTML
     // ELEMENT.appendChild
@@ -45,15 +54,15 @@ $.ajax(options);
 
 // dates js
 
-$(function () {
-    $('#datetimepicker6').datetimepicker();
-    $('#datetimepicker7').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker6").on("dp.change", function (e) {
-        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker7").on("dp.change", function (e) {
-        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-    });
-});
+// $(function () {
+//     $('#datetimepicker6').datetimepicker();
+//     $('#datetimepicker7').datetimepicker({
+//         useCurrent: false //Important! See issue #1075
+//     });
+//     $("#datetimepicker6").on("dp.change", function (e) {
+//         $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+//     });
+//     $("#datetimepicker7").on("dp.change", function (e) {
+//         $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+//     });
+// });
